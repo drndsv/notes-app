d44a3f29388e61195cebb229f083ed32ce64142e
dashk <burckina.anna@yandex.ru>
2026-03-30 17:04:06 +0300

    feat(services): добавлен LocalStorageNotesService

---
+++ notes-app/src/app/services/local-storage-notes.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Note } from '../interfaces/note.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageNotesService {
+  private readonly storageKey = 'notes';
+
+  getNotes(): Observable<Note[]> {
+    const notes = localStorage.getItem(this.storageKey);
+
+    if (!notes) {
+      return of([]);
+    }
+
+    return of(JSON.parse(notes) as Note[]);
+  }
+
+  addNote(note: Omit<Note, 'id'>): Observable<Note> {
+    const notes = this.getNotesFromStorage();
+
+    const newNote: Note = {
+      id: this.generateId(notes),
+      ...note,
+    };
+
+    const updatedNotes = [...notes, newNote];
+    localStorage.setItem(this.storageKey, JSON.stringify(updatedNotes));
+
+    return of(newNote);
+  }
+
+  private getNotesFromStorage(): Note[] {
+    const notes = localStorage.getItem(this.storageKey);
+    return notes ? (JSON.parse(notes) as Note[]) : [];
+  }
+
+  private generateId(notes: Note[]): number {
+    if (notes.length === 0) {
+      return 1;
+    }
+
+    return Math.max(...notes.map((item) => item.id)) + 1;
+  }
+}
