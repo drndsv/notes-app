88eb214ecef770ba54897b8f811798b98a4cb447
dashk <burckina.anna@yandex.ru>
2026-03-30 18:28:01 +0300

    feat(di): добавлена настройка DI-провайдера для выбора сервиса заметок через factory

--- notes-app/src/app/app.config.ts
+++ notes-app/src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LocalStorageNotesService } from './services/local-storage-notes.service';
+import { NotesApiService } from './services/notes-api.service';
+import { NotesSourceService } from './services/notes-source.service';
+import { NOTES_SERVICE } from './tokens/notes-service.token';
 
 
 export const appConfig: ApplicationConfig = {
@@ -10,5 +14,18 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
+    {
+      provide: NOTES_SERVICE,
+      useFactory: () => {
+        const notesSourceService = inject(NotesSourceService);
+        const source = notesSourceService.getSource();
+
+        if (source === 'localStorage') {
+          return inject(LocalStorageNotesService);
+        }
+
+        return inject(NotesApiService);
+      },
+    }
   ]
 };
--- notes-app/src/app/app.ts
+++ notes-app/src/app/app.ts
@@ -1,12 +1,64 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { WA_LOCATION } from '@ng-web-apis/common';
+
+import { Note } from './interfaces/note.interface';
+import { NotesServiceInterface } from './interfaces/notes-service.interface';
+import { NotesSourceService } from './services/notes-source.service';
+import { NOTES_SERVICE } from './tokens/notes-service.token';
+import { NotesSource } from './types/notes-source.types';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.less',
 })
-export class App {
-  protected readonly title = signal('notes-app');
+export class App implements OnInit {
+  private readonly notesService = inject<NotesServiceInterface>(NOTES_SERVICE);
+  private readonly location = inject(WA_LOCATION);
+
+  protected readonly notesSourceService = inject(NotesSourceService);
+
+  notes: Note[] = [];
+  title = '';
+  content = '';
+  selectedSource: NotesSource = this.notesSourceService.getSource();
+
+  ngOnInit(): void {
+    this.loadNotes();
+  }
+
+  loadNotes(): void {
+    this.notesService.getNotes().subscribe((notes) => {
+      this.notes = notes;
+    });
+  }
+
+  addNote(): void {
+    const trimmedTitle = this.title.trim();
+    const trimmedContent = this.content.trim();
+
+    if (!trimmedTitle || !trimmedContent) {
+      return;
+    }
+
+    this.notesService
+      .addNote({
+        title: trimmedTitle,
+        content: trimmedContent,
+      })
+      .subscribe(() => {
+        this.title = '';
+        this.content = '';
+        this.loadNotes();
+      });
+  }
+
+  changeSource(source: NotesSource): void {
+    this.notesSourceService.setSource(source);
+    this.location.reload();
+  }
 }
